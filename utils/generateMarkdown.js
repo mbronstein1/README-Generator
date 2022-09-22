8bcd3571b8a7cd1ebdf80f4a2d50c754dedf0ba0
Matthew Bronstein <mbronstein1@gmail.com>
2022-09-22 16:26:44 -0500

    removed a couple comments

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,5 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-
-//[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
-//[![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
-//[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
 function renderLicenseBadge(license) {
   switch(license) {
     case "MIT": license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
