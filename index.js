9c1506ff315b52300a5b4e91dc1bc7a6e39fce15
Matthew Bronstein <mbronstein1@gmail.com>
2022-09-22 16:17:17 -0500

    finished project including template literal markdown template and license logic

--- index.js
+++ index.js
@@ -7,49 +7,57 @@ const generateMarkdown = require("./utils/generateMarkdown");
 const questions = [
     {
         type: "input",
-        message: "Please provide a title for your application:",
+        message: "Please provide a title for your application (*required):",
         name: "title",
+        validate: (input) => input ? true : console.log("Please enter a title")
     },
     {
         type: "input",
-        message: "Please provide a description for your application:",
+        message: "Please provide a description for your application (*required):",
         name: "description",
+        validate: (input) => input ? true : console.log("Please enter a description")
     },
     {
         type: "input",
         message: "Please provide any installation instructions for your application:",
-        name: "installation"
+        name: "installation",
+        default: "N/A",
     },
     {
         type: "input",
         message: "Please provide any information that will help others use your application:",
         name: "usage",
+        default: "N/A",
     },
     {
         type: "list",
-        message: "If interested in licensing your application, please select one below:",
+        message: "Please select one below. If not interested in licensing, select 'none':",
         name: "license",
-        choices: ["MIT", "GPLv3", "Apache", "none"],
+        choices: ["MIT", "GPL v3", "Apache", "none"],
     },
     {
         type: "input",
         message: "Please provide guidelines for how to contribute to your application:",
         name: "contribution",
+        default: "N/A",
     },
     {
         type: "input",
         message: "Please provide any testing instructions to assist users:",
         name: "testing",
+        default: "N/A",
     },
     {
         type: "input",
-        message: "Please provide your GitHub username:",
+        message: "Please provide your GitHub username (*required):",
         name: "github",
+        validate: (input) => input ? true : console.log("Please enter your Github profile name"),
     },
     {
         type: "input",
-        message: "Please provide a preferred email address:",
+        message: "Please provide a preferred email address (*required):",
         name: "email",
+        validate: (input) => input ? true : console.log("Please enter your email address"),
     },
 ];
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,38 +1,105 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {
-  switch(license) {
-    case "MIT": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-      break;
-    case "GPL v3": "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
-      break;
-    case "Apache": "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
-      break;
-    case "none": "";
-      break;
-  }
-}
-
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown({title, description, installation, usage, license, contribution, testing, github, email}) {
-  return `
-${renderLicenseBadge(license)}
-# ${title}
-
-`;
-}
-
-module.exports = generateMarkdown;
 
 //[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)
 //[![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 //[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+function renderLicenseBadge(license) {
+  switch(license) {
+    case "MIT": license = "[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+      break;
+    case "GPL v3": license = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)";
+      break;
+    case "Apache": license = "[![License: Apache](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+      break;
+    case "none": license = "";
+      break;
+  }
+  return license
+}
 
+// TODO: Create a function that returns the license link
+// If there is no license, return an empty string
+function renderLicenseLink(license) {
+  switch(license) {
+    case "MIT": license = "Learn more [here](https://opensource.org/licenses/MIT).";
+      break;
+    case "GPL v3": license = "Learn more [here](https://www.gnu.org/licenses/gpl-3.0).";
+      break;
+    case "Apache": license = "Learn more [here](https://opensource.org/licenses/Apache-2.0).";
+      break;
+    case "none": license = "";
+      break;
+  }
+  return license;
+}
+
+// TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) {
+  switch(license) {
+    case "MIT": license = `> **Note**: This application is covered under the ${license} license.`;
+      break;
+    case "GPL v3": license = `> **Note** This application is covered under the ${license} license.`;
+      break;
+    case "Apache": license = `> **Note** This application is covered under the ${license} license.`;
+      break;
+    case "none": license = "N/A";
+      break;
+  }
+  return license;
+}
+
+// ${renderLicenseBadge(license)}
+// TODO: Create a function to generate markdown for README
+function generateMarkdown({title, description, installation, usage, license, contribution, testing, github, email}) {
+  return `
+  # ${title} ${renderLicenseBadge(license)}
+
+## Table of Contents
+- [Description](#description)
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Description
+${description}
+
+[Back to top](#)
+
+## Installation
+${installation}
+
+[Back to top](#)
+
+## Usage
+${usage}
+
+[Back to top](#)
+
+## License
+${renderLicenseSection(license)} ${renderLicenseLink(license)}
+
+[Back to top](#)
+
+## Contributing
+${contribution}
+
+[Back to top](#)
+
+## Tests
+${testing}
+
+[Back to top](#)
+
+## Questions
+You can view my GitHub repository [here](https://www.github.com/${github}). If you have any additional questions, feel free to reach out to me via [email](mailto:${email}).
+
+[Back to top](#)
+`;
+}
+
+module.exports = generateMarkdown;
